ebfee8c24c651a5da3eeec5448c30212a0bfafdc
vladkanmsk <vladkanmsk@icloud.com>
2025-06-29 18:55:35 +0300

    sdf

---
+++ konv.js
@@ -0,0 +1,12 @@
+let c = {'usd':'41', 'uah':'1'};
+let val = document.getElementById('val');
+let uah = document.getElementById('uah')
+let result = document.getElementsByClassName('convert_result');
+function summ(){
+    let z = 0;
+    if (val == uah){
+result.innerText == val.value;
+} 
+}
+
+// document.getElementById ("konvertor").textContent = parseInt(konvertation.replaceAll(' ', ''))/4
