719c0fcf8d2797341432a776cb3b2159f9a468fb
NikitaKikita007 <119515103+NikitaKikita007@users.noreply.github.com>
2025-06-30 16:52:56 +0300

    Improve currency converter input handling and UI
    Updated input types to 'number' for better validation, set a default value for USD input, and added an event listener for real-time conversion. Also added a currency select dropdown and moved the script tag to the HTML file for proper script loading.

--- konv.js
+++ konv.js
@@ -1,12 +1,19 @@
-let c = {'usd':'41', 'uah':'1'};
-let val = document.getElementById('val');
-let uah = document.getElementById('uah')
-let result = document.getElementsByClassName('convert_result');
-function summ(){
-    let z = 0;
-    if (val == uah){
-result.innerText == val.value;
-} 
+let c = { usd: "41", uah: "1" };
+let val = document.getElementById("val");
+let result = document.getElementById("uah");
+function summ() {
+  let z = 0;
+  if (valuta == "usd"){
+      if (val.value) {
+        result.value = val.value * c.usd;
+      }
+  }else if () {
+    
+  }
 }
-
+val.addEventListener("input", () => {
+  if (val.value) {
+    result.value = val.value * c.usd;
+  }
+});
 // document.getElementById ("konvertor").textContent = parseInt(konvertation.replaceAll(' ', ''))/4
